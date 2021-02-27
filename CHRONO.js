d9d324ace4afe9a0dec7a33d17f42b9adb302462
ikram2210 <ikramkhlif20@gmail.com>
2021-02-27 17:09:23 +0100

    ajouter police

--- CHRONO.js
+++ CHRONO.js
@@ -1,28 +1,25 @@
 jQuery(function($){
-var launch = new Date(2021,2,22,17,30,0);
-var Days = $('#Days');
-var Hours = $('#Hours');
-var Minutes = $('#Minutes');
-var Secondes = $('#Secondes');
-setDate();
-function setDate(){
-	var now = new Date();
-	var s = (launch.getTime() - now.getTime())/1000;
-	var d = Math.floor(s/86400);
-	Days.html('<strong>'+d+'</strong>');
-	s -= d*86400;
-	var h = Math.floor(s/3600);
-	Hours.html('<strong>'+h+'</strong>');
-	s -= h*3600;
-	var m = Math.floor(s/60);
-	Minutes.html('<strong>'+m+'</strong>');
-	s -= m*60;
-	var s = Math.floor(s);
-	Secondes.html('<strong>'+s+'</strong>');
-	setTimeout(setDate,1000);
+	var fformation = new Date(2021,2,22,17,30,0);
+	var Days = $('#Days');
+	var Hours = $('#Hours');
+	var Minutes = $('#Minutes');
+	var Secondes = $('#Secondes');
+	chrono();
+    function chrono()
+   {
+		var ajour = new Date();
+		var s = (fformation.getTime() - ajour.getTime())/1000;
+		var d = Math.floor(s/86400);
+		Days.html('<strong>'+d+'</strong>');
+		s -= d*86400;
+		var h = Math.floor(s/3600);
+		Hours.html('<strong>'+h+'</strong>');
+		s -= h*3600;
+		var m = Math.floor(s/60);
+		Minutes.html('<strong>'+m+'</strong>');
+		s -= m*60;
+		var s = Math.floor(s);
+		Secondes.html('<strong>'+s+'</strong>');
+		setTimeout(chrono,1000);
 	}
-
-	
-	
-
 });
